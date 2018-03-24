d5956ebee29b42357ea567c44d913452bee110ef
Amit Gupta <amitgupta.gwl@gmail.com>
2018-03-24 15:29:11 +0530

    fix HttpAsked syntax error

--- src/HttpAsked.js
+++ src/HttpAsked.js
@@ -3,14 +3,19 @@ var url = require('url');
 HttpAsked.prototype.toString = ()  => "Request (" + this.id + ") : " + this.url;
 
 function HttpAsked(request,params){
-    this.id = request.id,
-    //Constructing query param will slow down throughput by .4-.5k rps
-    //var parsedURL = url.parse(request.url, true);
-    //this.url =parsedURL.pathname,//without query param
-    //this.query = parsedURL.query,//convert into map
-    this.params = params,
-    this.nativeRequest = request,
+    processQueryParam(request);
+    this.id = request.id;
+    this.params = params;
+    this.nativeRequest = request;
     this.body =[]
 }
 
+ //Constructing query param will slow down throughput by .4-.5k rps
+function processQueryParam(request){
+    if( request.url.indexOf("?") !== -1 ){
+        var parsedURL = url.parse(request.url, true);
+        this.url =parsedURL.pathname//without query param
+        this.query = parsedURL.query//convert into map
+    }
+}
 module.exports = HttpAsked;
