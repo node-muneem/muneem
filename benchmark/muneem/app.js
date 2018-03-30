79be38646802468470628b1cc65456ee6c068120
Amit Gupta <amitgupta.gwl@gmail.com>
2018-03-30 13:19:54 +0530

    fix perf script

--- benchmark/muneem/app.js
+++ benchmark/muneem/app.js
@@ -1,45 +1,49 @@
 const path = require("path");
 //var uniqid = require('uniqid');
 
+
 const Muneem = require("../../src/muneem");
 //Muneem.setLogger(console);
 //Muneem.logger.log.info("test internal logger")
 
 Muneem.addToAnswer("justForFun", function(msg){
-    this.data = "justforfun : " + msg;
+    this.write("justforfun : " + msg);
 } );
 
 const muneem = Muneem({
-    mappings : path.join(__dirname, "mappings.yaml"),
-    //alwaysReadRequestPayload : true
+    //mappings : path.join(__dirname, "mappings.yaml"),
+    //alwaysReadRequestPayload : true,
+    //idGenerator : uniqid
 });
 
-muneem.addHandler("preStream", {
+
+
+/* muneem.addHandler("stream", {
     handle : () => {}
-}, { handlesStream : true});
-
-muneem.addHandler("parallel", (req,answer) => {
-    //console.log("parallel")
-},{
-    inParallel : true
 });
+ */
+/* muneem.addHandler("parallel", (req,answer) => {
+    setTimeout(() => {
+        cconsole.log("async")
+    },0)
+}); */
 muneem.addHandler("main", (req,answer) => {
-    //answer.data = '{ "hello" : "world" }';
     answer.write('{ "hello" : "world" }');
+    //answer.write(req.body);
+    answer.justForFun("test");
     //answer.justForFun("test");
-    //answer.justForFun("test");
-    //console.log("main")
 });
-muneem.addHandler("post", (req,answer) => {
+/* muneem.addHandler("post", (req,answer) => {
     //console.log("post")
-});
+}); */
+muneem.route({
+    uri: "/sample",
+    to: "main"
+})
 
 /* muneem.beforeEachHandler(() => {
     //console.log("before")
 }); */
 
-const server = muneem.createServer({
-    //idGenerator : uniqid
-});
+muneem.start();
 //console.log(muneem.routesManager.router.routes);
-server.start();
