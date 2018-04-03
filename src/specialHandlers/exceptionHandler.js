5cf07b44a4e83fb3fe4f686ef4f7189b87c97489
Amit Gupta <amitgupta.gwl@gmail.com>
2018-04-03 10:25:50 +0530

    update message code in case of server error

--- src/specialHandlers/exceptionHandler.js
+++ src/specialHandlers/exceptionHandler.js
@@ -3,7 +3,7 @@ const logger = require("./../fakeLogger");
 
 exports.handle = function(asked,answer){
     const error = answer.error;
-    answer.status(500);
-    answer.close("Internal Application Error");
+    answer.status(500, "Internal Server Error");
+    answer.close("Internal Server Error");
     logger.log.error(error,asked.route);
 }
