2ed70737e20fd6b539b22ee74e3a5993f26a0cd1
Amit Gupta <amitgupta.gwl@gmail.com>
2019-02-19 11:42:08 +0530

    Handle the scenario when error object is not passed

--- src/defaultHandlers/exceptionHandler.js
+++ src/defaultHandlers/exceptionHandler.js
@@ -1,6 +1,6 @@
 const logger = require("./../fakeLogger");
 
 module.exports = function(error, asked,answer){
-    answer.close(500, error.msg || "Internal Server Error");
+    answer.close(500, (error && error.msg) || "Internal Server Error");
     logger.log.error(error, asked.route);
 }
